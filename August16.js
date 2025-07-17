63f59e218abc2c1020435a47c8446546955dad9d
Aya Merghani <aya.a.h.merghani@gmail.com>
2025-07-17 21:16:56 +0400

    Add affirmations

--- August16.js
+++ August16.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
   var envelope = $("#envelope");
   var btn_open = $("#open");
   var btn_reset = $("#reset");
+  var non = $("#no");
 
   envelope.click(function () {
     open();
@@ -12,6 +13,9 @@ $(document).ready(function () {
   btn_reset.click(function () {
     close();
   });
+  non.click(function () {
+  Show();
+ });
 
   function open() {
     envelope.addClass("open").removeClass("close");
@@ -20,3 +24,22 @@ $(document).ready(function () {
     envelope.addClass("close").removeClass("open");
   }
 });
+ function Show() {
+  document.getElementById("nuh-uh").style.display = "block";
+ };
+
+  window.addEventListener('load', () => {
+    confetti({
+      particleCount: 100,
+      spread: 100,
+      origin: { y: 0.6 } // drop from a bit lower
+    });
+  });
+
+document.querySelectorAll('.strip').forEach(strip => {
+  strip.addEventListener('click', () => {
+    strip.classList.toggle('active');
+  });
+});
+
+
